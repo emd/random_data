6691ff5566b8429e01522d9b57124398b5723d30
Evan Davis <emd@mit.edu>
2015-02-21 12:53:48 -0800

    Setup presentation now more logical

--- setup.py
+++ setup.py
@@ -4,16 +4,14 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    'description': 'My Project',
-    'author': 'Evan M. Davis',
-    'url': 'URL to get it at.',
-    'download_url': 'Where to download it.',
-    'author_email': 'emd@mit.edu',
+    'name': 'projectname',
     'version': '0.1',
-    'install_requires': ['nose'],
     'packages': ['NAME'],
-    'scripts': [],
-    'name': 'projectname'
+    'install_requires': ['nose'],
+    'author': 'Evan M. Davis',
+    'author_email': 'emd@mit.edu',
+    'url': 'URL to get it at.',
+    'description': 'My Project'
 }
 
 setup(**config)
