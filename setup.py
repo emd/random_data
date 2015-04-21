ba981466d0108f2fea7ef19b0dd1f63ad0c84f77
emd <emd@mit.edu>
2015-04-21 14:44:06 -0700

    Updating install requirements

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ config = {
     'name': 'random_data',
     'version': '0.1',
     'packages': ['random_data'],
-    'install_requires': ['nose'],
+    'install_requires': ['numpy', 'matplotlib', 'event_manager', 'nose'],
     'author': 'Evan M. Davis',
     'author_email': 'emd@mit.edu',
     'url': '',
