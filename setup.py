dea2a7e2722afe5f895fed74b196fd99a9c03cc1
emd <emd@mit.edu>
2016-07-26 15:38:13 -0700

    Bumped version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 config = {
     'name': 'random_data',
-    'version': '0.2',
+    'version': '0.3',
     'packages': ['random_data'],
     'install_requires': ['numpy', 'matplotlib', 'nose'],
     'author': 'Evan M. Davis',
