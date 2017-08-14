37a9e957eb7b6ca6b9b2de40800816c73ed54f5f
emd <emd@mit.edu>
2017-08-14 02:17:23 -0700

    First cut at 2d spectra; Fourier implementation; some auto testing

---
+++ random_data/spectra/two_dimensional.py
@@ -0,0 +1,205 @@
+'''This module defines a class for estimating the 2-dimensional
+autospectral density of a field. Temporal spectral estimates are
+obtained through Welch's method of ensemble averaging overlapped,
+windowed FFTs (i.e. nonparametric spectral estimation), while
+spatial spectral estimates can be obtained through either
+nonparametric (FFT-based) or parametric (Burg autoregression)
+means.
+
+'''
+
+
+# Standard library imports
+import string
+import numpy as np
+from matplotlib import mlab
+
+# Intra-package imports
+from .parametric import BurgAutoSpectralDensity
+from .nonparametric import _plot_image
+from ..array import SpatialCrossCorrelation
+
+
+default_fourier_params = {
+    'window': np.hanning
+}
+
+default_burg_params = {
+    'p': 5,
+    'Nxi': 100
+}
+
+
+class TwoDimensionalAutoSpectralDensity(object):
+    '''A class for estimating the 2-dimensional autospectral density
+    of a field given the complex-valued spatial correlation function,
+    which e.g. can be computed from an array of measurements.
+
+    Attributes:
+    -----------
+
+    Methods:
+    --------
+    Type `help(TwoDimensionalAutoSpectralDensity)` in the IPython console
+    for a listing.
+
+    '''
+    def __init__(
+            self, corr, spatial_method='burg',
+            burg_params=default_burg_params,
+            fourier_params=default_fourier_params):
+        '''Create an instance of `TwoDimensionalAutoSpectralDensity` class.
+
+        Input parameters:
+        -----------------
+        corr - :py:class:`SpatialCrossCorrelation
+                <random_data.array.SpatialCrossCorrelation>` instance
+            A `SpatialCrossCorrelation` instance that characterizes
+            the complex-valued, spatial correlation function of the
+            process under study. Typically, the correlation function
+            is derived from cross-correlating an array of sensors
+            that are all measuring the same field.
+
+        spatial_method - string
+            The method to use when estimating the spatial spectral
+            density. Valid methods are:
+
+                - 'fourier': use FFT-based estimation, or
+                - 'burg': use Burg AR estimation.
+
+            Specification of other values will raise a ValueError.
+
+        burg_params - dict
+            A dictionary containing the parameters of relevance
+            for Burg spectral estimation. Valid dictionary keys
+            are: {'p', 'Nxi'} where
+
+            - p: int, order of Burg AR spectral-density estimate,
+            - Nxi: int, number of points in the two-sided *spatial*
+                spectral-density estimate at each frequency.
+
+            See documentation for :py:class:`BurgAutoSpectralDensity
+            <random_data.spectra.parametric.BurgAutoSpectralDensity>`
+            for more information on these parameters.
+
+        fourier_params - dict
+            A dictionary containing the parameters of relevance
+            for Fourier spectral estimation. Valid dictionary keys
+            are: {'window'} where
+
+            - window: tapering function to be applied to spatial
+                dimension of correlation function prior to calculating
+                the FFT; should be a function of the window length,
+                such as `np.hanning`. If `None`, do not apply a window
+                to the spatial dimension of the correlation function.
+                Although the correlation function typically smoothly
+                tapers to zero on its own, the application of a
+                window can still suppress leakage, at the cost of
+                marginally decreased resolution.
+
+        '''
+        # Check that user-provided `corr` is correct type
+        if type(corr) is not SpatialCrossCorrelation:
+            raise ValueError(
+                '`corr` must be of type %s'
+                % SpatialCrossCorrelation)
+
+        # Parse requested spatial method
+        self.spatial_method = string.lower(spatial_method)
+        implemented_spatial_methods = ['fourier', 'burg']
+
+        if self.spatial_method not in set(implemented_spatial_methods):
+            raise ValueError(
+                '`spatial_method` must be in %s'
+                % implemented_spatial_methods)
+
+        # Record important aspects of the computation
+        self.Fs = corr.Fs
+        self.Fs_spatial = 1. / (corr.separation[1] - corr.separation[0])
+
+        self.Npts_per_real = corr.Npts_per_real
+        self.Nreal_per_ens = corr.Nreal_per_ens
+        self.Npts_overlap = corr.Npts_overlap
+        self.Npts_per_ens = corr.Npts_per_ens
+        self.Nens = corr.Nens
+
+        self.detrend = corr.detrend
+        self.window = corr.window
+
+        self.f = corr.f
+        self.df = corr.df
+
+        self.t = corr.t
+        self.dt = np.nan
+
+        if self.spatial_method == 'burg':
+            self.p = burg_params['p']
+            self.Nxi = burg_params['Nxi']
+        elif self.spatial_method == 'fourier':
+            self.window_spatial = fourier_params['window']
+
+        # Estimate autospectral density
+        if self.spatial_method == 'burg':
+            pass
+        elif self.spatial_method == 'fourier':
+            self._getFourierSpectralDensity(corr)
+
+    def _getFourierSpectralDensity(self, corr):
+        '''Get 2-d autospectral density estimate by Fourier transforming
+        the complex-valued, spatial correlation function `corr`.
+
+        '''
+        Fs_spatial = 1. / (corr.separation[1] - corr.separation[0])
+        Npts = len(corr.separation[corr._valid])
+
+        if self.window_spatial is not None:
+            w = self.window_spatial(Npts)
+
+            # Normalize the window such that it has
+            # equivalent power to a window of ones
+            # with the same length.
+            power_loss = np.sum(np.abs(w) ** 2) / Npts
+            w /= np.sqrt(power_loss)
+        else:
+            w = np.ones(Npts)
+
+        # The spectral density is the Fourier transform
+        # of the correlation function. However, note that
+        # the Fourier transform X(f) of signal x(t) is related
+        # to the FFT of x(t) via:
+        #
+        #       X(f) = (1 / Fs) * FFT[x(t)](f),
+        #
+        # where Fs is the sampling rate of x(t).
+        #
+        # Further, recall that `corr.Gxy` has already been
+        # Fourier analyzed in time, so we only need to
+        # Fourier transform in space to obtain the estimated
+        # autospectral density.
+        self.Sxx = (1. / Fs_spatial) * np.fft.fftshift(
+            np.fft.fft(w[:, np.newaxis] * corr.Gxy[corr._valid], axis=0),
+            axes=0)
+
+        # Construct grid for spatial spectral density.
+        # Note that xi = (1 / wavelength) such that the
+        # wavenumber k is related to xi via k = 2 * pi * xi.
+        self.xi = np.fft.fftshift(np.fft.fftfreq(
+            Npts, d=(1. / Fs_spatial)))
+
+        self.dxi = self.xi[1] - self.xi[0]
+
+        return
+
+    def plotSpectralDensity(self, xilim=None, flim=None, vlim=None,
+                            cmap='viridis', interpolation='none', fontsize=16,
+                            title=None, xlabel=r'$\xi$', ylabel='$f$',
+                            cblabel=r'$|G_{xx}(\xi,f)|$',
+                            ax=None, fig=None, geometry=111):
+        'Plot magnitude of spectral density on log scale.'
+        ax = _plot_image(
+            self.xi, self.f, np.abs(self.Sxx.T),
+            xlim=xilim, ylim=flim, vlim=vlim,
+            norm='log', cmap=cmap, interpolation=interpolation,
+            title=title, xlabel=xlabel, ylabel=ylabel, cblabel=cblabel,
+            fontsize=fontsize,
+            ax=ax, fig=fig, geometry=geometry)
---
+++ tests/spectra_tests/two_dimensional_tests.py
@@ -0,0 +1,86 @@
+from nose import tools
+import numpy as np
+from random_data.array import SpatialCrossCorrelation
+from random_data.spectra.two_dimensional import (
+    TwoDimensionalAutoSpectralDensity)
+
+
+# Construct signal from which to initialize `SpatialCrossCorrelation`:
+# ====================================================================
+#
+# Temporal grid:
+# --------------
+Fs = 1.
+t0 = 0
+T = 10000
+t = np.arange(t0, T, 1. / Fs)
+
+# Spatial grid:
+# -------------
+Fs_spatial = 1.
+z0 = 0
+Ztot = 10
+z = np.arange(z0, Ztot, 1. / Fs_spatial)
+
+# 2d computational grids:
+# -----------------------
+tt = np.outer(np.ones(len(z)), t)
+zz = np.outer(z, np.ones(len(t)))
+
+# Coherent signal in presence of white noise:
+# -------------------------------------------
+A = 1.
+f0 = 0.1
+xi0 = 0.25
+x = A * np.cos(2 * np.pi * ((xi0 * zz) + (f0 * tt)))
+x += np.random.rand(x.shape[0], x.shape[1])
+x -= np.mean(x)  # avoid low-f, low-xi leakage
+
+# Compute `SpatialCrossCorrelation` object:
+# =========================================
+corr = SpatialCrossCorrelation(
+    x, z, Fs=Fs, t0=t0,
+    Tens=(0.5 * T), Nreal_per_ens=100)
+
+
+def test_TwoDimensionalAutoSpectralDensity_ValueError():
+    # Must pass `SpatialCrossCorrelation` object, not array
+    tools.assert_raises(
+        ValueError,
+        TwoDimensionalAutoSpectralDensity,
+        corr.Gxy)
+
+    # Must use a method that has been implemented
+    tools.assert_raises(
+        ValueError,
+        TwoDimensionalAutoSpectralDensity,
+        corr, **{'spatial_method': 'ARMA'})
+
+    return
+
+
+def test_TwoDimensionalAutoSpectralDensity_Fourier():
+    # Estimate 2d autospectral density via Fourier method:
+    # ----------------------------------------------------
+    asd2d = TwoDimensionalAutoSpectralDensity(
+        corr, spatial_method='fourier',
+        fourier_params={'window': np.hanning})
+
+    # Test that peak appears in right place:
+    # --------------------------------------
+    peak = np.max(np.abs(asd2d.Sxx))
+    ind = np.where(np.abs(asd2d.Sxx) == peak)
+
+    # Passing implies accuracy within 5%
+    xi_ratio = asd2d.xi[ind[0]] / xi0
+    np.testing.assert_almost_equal(xi_ratio, 1, decimal=1)
+
+    # Passing implies accuracy within 5%
+    f_ratio = asd2d.f[ind[1]] / f0
+    np.testing.assert_almost_equal(f_ratio, 1, decimal=1)
+
+    # Test power conservation:
+    # ------------------------
+    # ???
+
+    return
